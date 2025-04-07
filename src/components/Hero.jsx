21777dca7011bf916741b8caa50701b83e20c945
Elod <elodverbovci5@gmail.com>
2025-04-07 20:30:33 +0200

    Last fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
        <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6 font-Roboto'>FullStack Web Developer</h1>
        <div className='flex justify-center items-center '>
         <p className='md:text-2xl sm:text-xl text-sm font-bold py-4 font-FiraCode'>Proficient in:</p>
-        <ReactTyped className='md:text-2xl sm:text-sm text-sm font-bold pl-2 text-[#00df9a] font-FiraCode' strings={['JavaSript,ReactJS,NodeJS']} typeSpeed={120} backSpeed={140} loop />
+        <ReactTyped className='md:text-2xl sm:text-sm text-sm font-bold pl-2 text-[#00df9a] font-FiraCode' strings={['JavaScript,ReactJS,NodeJS']} typeSpeed={120} backSpeed={140} loop />
        </div>
        <p className='md:text-1xl text-sm font-bold text-gray-500 md:pl-4 font-FiraMono my-4'>Delivering Fast , Precise , Robust Websites & Applications!</p>
       
